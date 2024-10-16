7e4421a2eb68d4469907e860ed3ec3f6dd38ff1e
Your Name <you@example.com>
2024-10-16 17:33:51 +0500

    kuch garbar hogai hai

--- backend/models.py
+++ backend/models.py
@@ -7,4 +7,15 @@ class User(Base):
     username = Column(String)
     email = Column(String)
     password = Column(String)
+
     
+
+class Staff(Base):
+    __tablename__ = "Staff"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    email = Column(String)
+    password = Column(String) 
+    type = Column(String)
+
+
